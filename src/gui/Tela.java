6ce14c5505e761ebcf5d2c12a7713ad9d41c6c4c
Alexandre Knabah <knabah.alexandre@gmail.com>
2023-11-10 13:49:04 -0300

    Icone adicionado

--- src/gui/Tela.java
+++ src/gui/Tela.java
@@ -1,4 +1,27 @@
 package gui;
 
-public class Tela {
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+public class Tela extends JFrame {
+
+    public Tela() throws IOException{
+        setTitle("Gerador Musical");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setIconImage(ImageIO.read(new File("img/nota.png")));
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        try {
+            new Tela();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
