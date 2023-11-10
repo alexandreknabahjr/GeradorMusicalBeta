9a22d1e37f4624461fe33784d8bfaf046d588edd
Alexandre Knabah <knabah.alexandre@gmail.com>
2023-11-10 15:16:54 -0300

    Icone adicionado

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import org.jfugue.player.Player;
 
 public class Main {
     public static void main(String[] args) {
-        SequenciaMusical seq = new SequenciaMusical("A???????A");
+        SequenciaMusical seq = new SequenciaMusical("C9CCCCC");
         String seqDecod = seq.decodificaSequencia();
         System.out.println(seqDecod);
         Player player = new Player();
--- src/gui/Tela.java
+++ src/gui/Tela.java
@@ -2,10 +2,13 @@ package gui;
 
 import javax.swing.*;
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
 public class Tela extends JFrame {
+
+    Botao botaoGeraMusica = new Botao();
 
     public Tela() throws IOException{
         setTitle("Gerador Musical");
@@ -13,6 +16,10 @@ public class Tela extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
+        setLayout(null);
+        botaoGeraMusica.setText("Gerar Música");
+        botaoGeraMusica.setBounds(220,250,150,50);
+        add(botaoGeraMusica);
         setIconImage(ImageIO.read(new File("img/nota.png")));
         setVisible(true);
     }
