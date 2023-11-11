a1a9ac35d9788ef2a8a17271628a384781b424e6
Alexandre Knabah <knabah.alexandre@gmail.com>
2023-11-11 13:25:54 -0300

    Configurações de Tela

--- src/gui/Tela.java
+++ src/gui/Tela.java
@@ -13,21 +13,29 @@ public class Tela extends JFrame{
     private CaixaTexto inputMusica;
 
     public Tela() throws IOException{
+        configTela();
+        adicionaComponentes(inputMusica, botaoGeraMusica);
+        setVisible(true);
+    }
+
+    private void configTela() throws IOException{
         setTitle("Gerador Musical");
         setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
         setLayout(null);
-        inputMusica = new CaixaTexto();
-        botaoGeraMusica = new Botao(inputMusica);
-        botaoGeraMusica.setText("Gerar Música");
-        botaoGeraMusica.setBounds(220,250,150,50);
-        inputMusica.setBounds(100, 100, 400, 100);
-        add(botaoGeraMusica);
-        add(inputMusica);
         setIconImage(ImageIO.read(new File("img/nota.png")));
-        setVisible(true);
+    }
+
+    private void adicionaComponentes(CaixaTexto caixa, Botao botao){
+        caixa = new CaixaTexto();
+        botao = new Botao(caixa);
+        botao.setText("Gerar Música");
+        botao.setBounds(220,250,150,50);
+        caixa.setBounds(100, 100, 400, 100);
+        add(botao);
+        add(caixa);
     }
 
     public static void main(String[] args) {
