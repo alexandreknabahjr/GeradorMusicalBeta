59c1b7b47b73e7783e28e4cb4bca428f2724a174
Matheus dos Anjos <matheusadosanjos@hotmail.com>
2023-12-29 11:08:10 -0300

    Modif Notas de String para char

--- src/musica/NotasMusicais.java
+++ src/musica/NotasMusicais.java
@@ -2,32 +2,27 @@ package musica;
 
 public enum NotasMusicais {
 
-    LA("A"),
-    SI("B"),
-    DO("C"),
-    RE("D"),
-    MI("E"),
-    FA("F"),
-    SOL("G");
+    LA('A'),
+    SI('B'),
+    DO('C'),
+    RE('D'),
+    MI('E'),
+    FA('F'),
+    SOL('G');
 
-    private String codNota;
+    private final char codNota;
 
-    NotasMusicais(String codNota){
+    NotasMusicais(char codNota){
         this.codNota = codNota;
     }
 
     @Override
     public String toString(){
-        return codNota;
+        return String.valueOf(codNota);
     }
-
-    public char toChar(){
-        return codNota.charAt(0);
-    }
-
     public static boolean contem(char codNotaInput) {
         for (NotasMusicais notasMusicais : values()) {
-            if (notasMusicais.codNota.equals(String.valueOf(codNotaInput))) {
+            if (notasMusicais.codNota == codNotaInput) {
                 return true;
             }
         }
